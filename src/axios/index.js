5efd38d06086ffbd4fb055a4f0426cc61f9289d1
Rama Krishna <ramakrishna@Rama-Krishna.local>
2019-09-02 21:42:32 +0530

    dynamic base url

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://whitepanda.cboxera.com/' : 'http://localhost:2019/';
+
 const instance = axios.create({
-    baseURL: 'http://localhost:2019/',
+    baseURL: BASE_URL,
     timeout: 1000,
   });
 
